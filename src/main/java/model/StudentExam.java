fe6399cae8d88f3f49699a79525d509b6bcb60fb
Louttche <lnicolaidou.katerina@gmail.com>
2021-10-30 12:14:18 +0200

    StudentExam - Create Tests (TEST SHOULD FAIL)

--- src/main/java/model/StudentExam.java
+++ src/main/java/model/StudentExam.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  * contains the student, the exam he/she took, classcode used and all screenshots, logging, uploaded examwork of a student which took an exam.
  *
@@ -8,9 +11,62 @@ package model;
  *
  */
 public class StudentExam {
-    //private Student student;
-    //private ExamID exam;
-    //private int classcode; //  are the ECs of the course, e.g., 3
+    private Student student;
+    private ExamID exam;
+    private String classcode; //  are the ECs of the course, e.g., 3
 
-    // TODO: the appropiate methods
+    private List<Object> student_material;
+
+    /**
+     *
+     * @param student
+     * @param examID
+     * @param classcode
+     *
+     * @should throw a NullPointerException if student is null
+     * @should throw a NullPointerException if examID is null
+     * @should throw a NullPointerException if classcode is null
+     */
+    public StudentExam(Student student, ExamID examID, String classcode){
+        this.student = student;
+        this.exam = examID;
+        this.classcode = classcode;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public ExamID getExam() {
+        return exam;
+    }
+
+    public void setExam(ExamID exam) {
+        this.exam = exam;
+    }
+
+    public String getClasscode() {
+        return classcode;
+    }
+
+    public void setClasscode(String classcode) {
+        this.classcode = classcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Course)) return false;
+        StudentExam studentExam = (StudentExam) o;
+        return this.student.equals(studentExam.student);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.student);
+    }
 }
